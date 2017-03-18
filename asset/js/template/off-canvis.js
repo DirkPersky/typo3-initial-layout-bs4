dc4c886251bf78eb326c3b3285d79de6f94d0be9
Dirk <dearic@gmx.net>
2017-03-18 22:48:47 +0100

    Above the Fold CSS Files-Vorbereitung

--- asset/js/template/off-canvis.js
+++ asset/js/template/off-canvis.js
@@ -25,12 +25,18 @@
       // If no Touch
       if(!$timer && !$megaNav) return true;
 
+      if ($childList.hasClass('hidden-sm-up') && window.matchMedia("(min-width: 544px)").matches) return true;
+      if ($childList.hasClass('hidden-md-up') && window.matchMedia("(min-width: 768px)").matches) return true;
+      if ($childList.hasClass('hidden-lg-up') && window.matchMedia("(min-width: 992px)").matches) return true;
+      if ($childList.hasClass('hidden-xl-up') && window.matchMedia("(min-width: 1200px)").matches) return true;
       // Stop Click when time is short;
       e.preventDefault();
       // Add Class to Parent
       if($childList.hasClass('on')) {
+        $this.removeClass('in-sub');
         $childList.removeClass('on');
       } else {
+        $this.addClass('in-sub');
         $childList.addClass('on');
       }
 
@@ -41,6 +47,7 @@
     // Remove Class Action
     $childList.find(' > ul > li > .btn-close').on('click',function(e){
       e.preventDefault();
+      $this.removeClass('in-sub');
       $childList.removeClass('on');
     })
   };
@@ -54,47 +61,81 @@
    * modify: 22.04.2016
    */
   $.fn.megaNav = function( optionsConf ) {
-    var $this = $(this);
-    var $child = jQuery($this.data('mega'));
+      var $this = $(this),
+          $child = jQuery($this.data('mega')),
+          _slideUpTimer,
+          _lockTimer;
 
-    $this.on('click',function(e){
-      if($child.length > 0) {
-        // Stop Click when time is short;
-        e.preventDefault();
+      $this.on('click mouseenter mouseleave', function (e) {
+          if ($child.length > 0) {
+              // Stop Click when time is short;
+              e.preventDefault();
+              clearTimeout(_slideUpTimer);
+              if (e.type == 'mouseleave') {
+                  _slideUpTimer = setTimeout(slideUp, 750);
+              } else if(e.type == 'mouseenter'){
+                  slideDown();
+                  _lockTimer = setTimeout(function () {
+                      _lockTimer = null;
+                  }, 50);
+              } else if (!_lockTimer) {
+                  if ($child.hasClass('on')) {
+                      slideUp();
+                  } else {
+                      slideDown();
+                  }
+              }
+          }
+      });
 
-        if($child.hasClass('on')) {
-          slideUp();
-        } else {
-          slideDown();
-        }
+      $('a[data-mega]').each(function () {
+          var _child = $($(this).data('mega'));
+          _child.on('mouseleave mouseenter', function (e) {
+              clearTimeout(_slideUpTimer);
+              if (e.type == 'mouseleave') {
+                  _slideUpTimer = setTimeout(slideUp, 500);
+              }
+          });
+      });
+
+      function slideUp() {
+          // $child.stop().slideUp('slow', function(){
+          $child.removeClass('on');
+          $this.removeClass('toogle-mega').blur();
+          // });
+
+          /** Find Active and rest **/
+          $('a[data-mega]').each(function () {
+              if ($(this).parent().hasClass('toogle-mega-holder')) {
+                  $(this).parent().removeClass('toogle-mega-holder');
+              }
+          });
       }
-    });
 
-    function slideUp(){
-      $child.stop().slideUp('slow', function(){
-        $child.removeClass('on');
-        $this.removeClass('toogle-mega');
-      });
-    }
+      function slideDown() {
+          $('a[data-mega]').each(function () {
+              var _child = $($(this).data('mega'));
+              $(this).removeClass('toogle-mega');
 
-    function slideDown(){
-      $('a[data-mega]').each(function(){
-        var _child = $($(this).data('mega'));
-        $(this).removeClass('toogle-mega');
+              // _child.stop().slideUp('slow', function(){
+              _child.removeClass('on');
+              // });
+          });
 
-        _child.stop().slideUp('slow', function(){
-          _child.removeClass('on');
-        });
-      });
+          // $child.stop().slideDown('slow', function(){
+          $child.addClass('on');
+          $this.addClass('toogle-mega').blur();
+          // })
 
-      $child.stop().slideDown('slow', function(){
-        $child.addClass('on');
-        $this.addClass('toogle-mega');
-      })
-    }
+          /** Find Active and Store **/
+          $('a[data-mega]').each(function () {
+              if ($(this).parent().hasClass('active')) {
+                  $(this).parent().addClass('toogle-mega-holder');
+              }
+          });
+      }
   };
   $('a[data-mega]').each(function(){
     $(this).megaNav();
   });
-
 }(jQuery);
