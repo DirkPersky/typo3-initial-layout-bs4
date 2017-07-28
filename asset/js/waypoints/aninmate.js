243cc5cde96442071d98d5f54170a64bc488c707
Dirk Persky <d.persky@gutenberghaus.de>
2017-07-28 15:55:47 +0200

    Simple Animation

--- asset/js/waypoints/aninmate.js
+++ asset/js/waypoints/aninmate.js
@@ -1,14 +1,14 @@
-!function($){
-  /**
-   * Aniamtion CSS on Scroll
-   */
-  $('.animated-hide').each(function(){
-    $(this).waypoint(function(direction){
-      this.adapter.$element.toggleClass('animated-hide')
-    },{
-      triggerOnce: true,
-      offset: '90%' // Way to top of Page
+jQuery(function($){
+    /**
+     * Aniamtion CSS on Scroll
+     */
+    $('.animated-hide').each(function(){
+        $(this).addClass('animated');
+        $(this).waypoint(function(direction){
+            if(direction == 'down') this.adapter.$element.removeClass('animated-hide')
+        },{
+            triggerOnce: true,
+            offset: '90%' // Way to top of Page
+        });
     });
-  });
-  
-}(jQuery);
+});
