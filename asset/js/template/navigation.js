d166875c6ed6b7d75f800904efa014895a3bfc94
Dirk Persky <d.persky@gutenberghaus.de>
2017-09-29 15:45:35 +0200

    Add Navbar Dropdown Handling

---
+++ asset/js/template/navigation.js
@@ -0,0 +1,32 @@
+jQuery(function($){
+    $.fn.dropDownMenu = function(){
+        this.map(function(index, el){
+            var self = $(el),
+                link = self.find(' > .nav-link'),
+                dropdown = self.find(' > .dropdown-wrap');
+            // Create Toggle Button
+            link.append('<span class="trigger"/>');
+            // Add Label to SubNav
+            dropdown.find('> ul').prepend('<li class="list-header">'+link.text()+'</li>');
+            // Add CLick Bindings
+            var btn = link.find('.trigger'),
+                header = dropdown.find('> ul > .list-header');
+            // Bind Open Handler
+            btn.on('click', (e) => {
+                e.preventDefault();
+                e.stopPropagation();
+
+                dropdown.addClass('show');
+            });
+            // Bind Close Handler
+            header.on('click', (e) => {
+                e.preventDefault();
+                e.stopPropagation();
+
+                dropdown.removeClass('show');
+            });
+        });
+    };
+
+    $('.nav-inline .dropdown').dropDownMenu();
+});
