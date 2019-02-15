99842248a7ec2109954c2187f57c520174b27700
Dirk Persky <d.persky@gutenberghaus.de>
2019-02-15 09:03:27 +0100

    Update AdditionalConfiguration.php

--- asset/typ3conf/AdditionalConfiguration.php
+++ asset/typ3conf/AdditionalConfiguration.php
@@ -44,3 +44,12 @@ switch ($system) {
         $GLOBALS['TYPO3_CONF_VARS']['SYS']['enableDeprecationLog'] = '';
         break;
 }
+/**
+ * Remove Chash
+ */
+$GLOBALS['TYPO3_CONF_VARS']['FE']['cacheHash']['excludedParameters'] = array_merge(
+    $GLOBALS['TYPO3_CONF_VARS']['FE']['cacheHash']['excludedParameters'],
+    [
+        'tx_news_pi1[@widget_0][currentPage]'
+    ]
+);
