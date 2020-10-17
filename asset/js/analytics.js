98f67c417af71ee6a651ca2cfe1d31c6a1cb0951
Dirk Persky <dirk.persky@gmail.com>
2020-10-17 21:34:04 +0200

    update for new sites

--- asset/js/analytics.js
+++ asset/js/analytics.js
@@ -1,10 +1,10 @@
 /**
-  * Update Analytics on Barba Reload
+ * Update Analytics on Barba Reload
  */
 window.Statemanager.attach('analytics-update', function() {
-    if (typeof window.dataLayer != 'function') {
-        window.dataLayer.some( set => {
-            if(set[0] == 'config') {
+    if (typeof window.dataLayer != 'function' && typeof window.dataLayer != 'undefined') {
+        window.dataLayer.some(set => {
+            if (set[0] == 'config') {
                 var config = set[2];
                 config.page_title = document.title;
                 config.page_path = (window.location.href).replace(window.location.origin, '').toLowerCase();
