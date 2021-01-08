0f06c1ffe311b92aab1274b527943386308386b8
Dirk Persky <dirk.persky@gmail.com>
2021-01-08 13:20:04 +0100

    Webpack update

--- webpack.config.js
+++ webpack.config.js
@@ -102,10 +102,7 @@ class WebpackConfig {
             test: /\.(sa|sc|c)ss$/,
             use: [
                 {
-                    loader: MiniCssExtractPlugin.loader,
-                    options: {
-                        hmr: process.env.NODE_ENV === 'development',
-                    },
+                    loader: MiniCssExtractPlugin.loader
                 },
                 'css-loader',
                 'sass-loader',
